131ee0fd2de643209b53df4b9bb5f71ad6b48c53
YakirHadad <54073831+yakir62@users.noreply.github.com>
2023-12-30 08:08:00 +0200

    Update list-users.sh

--- github-api/list-users.sh
+++ github-api/list-users.sh
@@ -4,12 +4,18 @@
 API_URL="https://api.github.com"
 
 # GitHub username and personal access token
-USERNAME=$username
-TOKEN=$token
+USERNAME="$username"
+TOKEN="$token"
 
 # User and Repository information
-REPO_OWNER=$1
-REPO_NAME=$2
+REPO_OWNER="$1"
+REPO_NAME="$2"
+
+# Check if required parameters are provided
+if [ -z "$REPO_OWNER" ] || [ -z "$REPO_NAME" ] || [ -z "$USERNAME" ] || [ -z "$TOKEN" ]; then
+    echo "Usage: $0 <repository_owner> <repository_name>"
+    exit 1
+fi
 
 # Function to make a GET request to the GitHub API
 function github_api_get {
@@ -25,7 +31,7 @@ function list_users_with_read_access {
     local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
 
     # Fetch the list of collaborators on the repository
-    collaborators="$(github_api_get "$endpoint" | jq -r '.[] | select(.permissions.pull == true) | .login')"
+    collaborators="$(github_api_get "$endpoint" | jq -r 'if type == "array" then .[] else . end | select(.permissions.pull == true) | .login')"
 
     # Display the list of collaborators with read access
     if [[ -z "$collaborators" ]]; then
